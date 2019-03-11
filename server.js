64ed8b1ab27cfe4ef4b20e8eb559ccc2b3ec0176
Anargyres <anargyres9278@gmail.com>
2019-03-11 14:56:47 +0100

    add entry point

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
 
+app.post('/chat', function(req, res) {
+  console.log(res)
+})
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
