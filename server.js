0e1aa74150382008344795228b7978c35466f043
Anargyres <anargyres9278@gmail.com>
2019-03-11 14:07:19 +0100

    change server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(3000);
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
