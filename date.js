4b5296dde76e58125186741e8eaa74938e96a7c2
Anargyres <anargyres9278@gmail.com>
2019-03-12 10:47:53 +0100

    print dates collections

--- date.js
+++ date.js
@@ -9,10 +9,26 @@ const assert = require('assert');
   const client = new MongoClient(url);
 
   try {
-    // Use connect method to connect to the Server
     await client.connect();
+    console.log("Connected correctly to server");
 
     const db = client.db(dbName);
+
+    // Get the collection
+    const col = db.collection('dates');
+
+    // Insert multiple documents
+    const r = await col.insertMany([{a:1}, {a:1}, {a:1}]);
+    assert.equal(3, r.insertedCount);
+
+    // Get the cursor
+    const cursor = col.find({a:1}).limit(2);
+
+    // Iterate over the cursor
+    while(await cursor.hasNext()) {
+      const doc = await cursor.next();
+      console.dir(doc);
+    }
   } catch (err) {
     console.log(err.stack);
   }
